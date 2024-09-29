991897f7d4420f8729d5b6b40f177ebe4ebde65f
spectropaws <spectropaws777@gmail.com>
2024-09-30 03:06:49 +0530

    handle socket io connections

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,9 +10,9 @@ const __dirname = dirname(__filename);
 // main imports
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
-const port = process.env.PORT || 8000;
+const port: string | Number = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json());
@@ -26,8 +26,29 @@ const io = new Server(server, {
     },
 });
 
-io.on("connection", (socket) => {
+// list of users
+const users: { [key: string]: string } = {};
+
+io.on("connection", async (socket: Socket) => {
     console.log("User connected: " + socket.id);
+
+    socket.on("joinroom", (username: string) => {
+        users[socket.id] = username;
+        io.emit("memberconnect", {user: username});
+    });
+
+    socket.on("message", (message: { user: string, message: string }) => {
+        socket.broadcast.emit("message", message);
+    });
+
+    socket.on("typing", (isTyping: boolean) => {
+        socket.broadcast.emit("typing", isTyping);
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("memberDisconnect", {user: users[socket.id]});
+        delete users[socket.id];
+    })
 });
 
 app.get("/", (req, res) => {
